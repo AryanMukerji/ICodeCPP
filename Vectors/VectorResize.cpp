300de9770196e189686ac31e5e84033a5ca75457
Aryan Mukerji <aryanmukerji@gmail.com>
2020-05-22 07:46:31 +0530

    vector :: resize() in C++ STL

--- Vectors/VectorResize.cpp
+++ Vectors/VectorResize.cpp
@@ -86,6 +86,8 @@ void SpecifiedIncreace()
 { 
     vector<int> vec;
 	
+	// 5 elements are inserted 
+    // in the vector
 	vec.push_back(1); 
     vec.push_back(2); 
     vec.push_back(3); 
@@ -94,17 +96,22 @@ void SpecifiedIncreace()
   
     cout << "\n\n Contents of vector before resizing : "
          << endl;
-		 
+	
+	// displaying the contents of  
+    // the vector before resizing
 	for (int i = 0; i < vec.size(); i++) 
         cout << " " << vec[i] << " "; 
   
     cout << endl;
-
+	
 	vec.resize(12, 9);
 	
+	// vector is resized
 	cout << " Contents of vector after resizing : " 
          << endl;
 	
+	// displaying the contents  
+    // of the vector after resizing
 	for (int i = 0; i < vec.size(); i++) 
         cout << " " << vec[i] << " ";
 }
