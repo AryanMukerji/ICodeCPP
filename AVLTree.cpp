9ad2d5aebc36a97cac70a69e7deac8e5981939c4
Aryan Mukerji <aryanmukerji@gmail.com>
2020-06-16 17:17:50 +0530

    AVL Tree In C++ (Minor Additions Day 2 ++)

--- AVLTree.cpp
+++ AVLTree.cpp
@@ -38,4 +38,16 @@ Node *newNode(int key) // New node creation
     return (node);
 }
 
-
+Node *rightRotate(Node *y) // Rotate right
+{
+    Node *x = y->left;
+    Node *T2 = x->right;
+    
+    x->right = y;
+    y->left = T2;
+    
+    y->height = max( height(y->left), height(y->right) ) + 1;
+    x->height = max( height(x->left), height(x->right) ) + 1;
+    
+    return x;
+}
