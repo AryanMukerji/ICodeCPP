1dc87aaf5dea0642ae89d17da7eab18e49d6d3da
Aryan Mukerji <aryanmukerji@gmail.com>
2020-06-21 21:09:33 +0530

    Binary Search Tree In C++ : Update 1.01

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -8,3 +8,25 @@ struct node
     struct node *left;
     struct node *right;
 }*root;
+
+class BST
+{
+    public:
+    
+    BST()
+    {
+        root = NULL;
+    }
+    
+    node * Find(node *, int);
+    void Search(int, node **, node **);    
+    void Insert(node *, node *);
+    void Delete(int);
+    void CaseA(node *, node *);
+    void CaseB(node *, node *);
+    void CaseC(node *, node *);
+    void Preorder(node *);
+    void Inorder(node *);
+    void Postorder(node *);
+    void Display(node *, int);
+};
