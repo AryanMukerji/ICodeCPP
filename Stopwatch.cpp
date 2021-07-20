c79b1897c4738235ae35e1a943714ba3ad8edc64
Aryan Mukerji <aryanmukerji@gmail.com>
2021-07-20 15:19:11 +0530

    Stopwatch Program in C++

---
+++ Stopwatch.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <unistd.h>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int H = 0, M = 0, S = 0;
+    
+    while (true)
+    {
+        system("clear");
+        
+        cout<<"\n\t\t\t\t"<< H <<" : "<< M <<" : "<< S <<"\n";
+        sleep(1);
+        
+        S++;
+        
+        if (S == 60)
+        {
+            M++;
+            S = 0;
+            
+            if (M == 60)
+            {
+                H++;
+                M = 0;
+                
+                if (H == 24)
+                {
+                    H = 0;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
